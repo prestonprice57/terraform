b622db1875bf33dcfcc4f02808a303dca9d4ab25
darkmoonsatellite1985 <115838985+darkmoonsatellite1985@users.noreply.github.com>
2024-01-17 17:05:03 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket" "test_a2aJdvb9QZ9zbjvF" {
+  bucket = "test"
+}
+
+resource "aws_s3_bucket_public_access_block" "test_a2aJdvb9QZ9zbjvF" {
+  bucket = aws_s3_bucket.test_a2aJdvb9QZ9zbjvF.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "test_a2aJdvb9QZ9zbjvF" {
+  bucket = aws_s3_bucket.test_a2aJdvb9QZ9zbjvF.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "test_a2aJdvb9QZ9zbjvF" {
+  bucket = aws_s3_bucket.test_a2aJdvb9QZ9zbjvF.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "test_a2aJdvb9QZ9zbjvF" {
+  bucket = aws_s3_bucket.test_a2aJdvb9QZ9zbjvF.id
+
+  rule {
+    id = "transition-to-glacier"
+    filter {}
+    status = "Enabled"
+    transition {
+      days          = 11
+      storage_class = "GLACIER"
+    }
+  }
+
+  rule {
+    id = "abort-multipart-upload"
+    filter {}
+    status = "Enabled"
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+}
